6e80f1437a71810ab579a0b3bcbee1d194093044
samckp <sam.mca.liet@gmail.com>
2020-08-15 10:17:39 +0530

    updated

---
+++ test.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello")
+
+	// for loop
+	for i := 0; i<=10 ; i++ {
+
+		fmt.Println(i)
+	}
+
+	//for loop with condition and continue
+	for j := 0; j<10;j++{
+		if(j%2==0){
+			continue
+		}
+			fmt.Println(j);
+	}
+}
